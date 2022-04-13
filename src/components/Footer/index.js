4b0950cf4ebb82a70dd2219a2bf2399cb792637a
Fadli Fadillah <fadli55fadillah@gmail.com>
2022-04-13 23:36:49 +0700

    Jawaban UTS Fadli

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,14 @@
+import styles from "./footer.module.css";
+
 function Footer() {
   return (
-    <>
-      <h2>Example Footer</h2>
-    </>
+    <div className={styles.container}>
+      <footer className={styles.footer}>
+        <h2 className={styles.footer__title}>Covid ID</h2>
+        <p className={styles.footer__author}>Created by aufaroot18</p>
+        <p className={styles.footer__author}>Develop by HAGESAN55</p>
+      </footer>
+    </div>
   );
 }
 
